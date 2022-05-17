f65d175b527442f0b95d05f72bd6508139696db5
Julian Snelders <104151791+FontysBoneZone@users.noreply.github.com>
2022-05-17 12:15:41 +0100

    .

--- main.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function() {
-    $(".menu").click(function() {
-      $(".nav_bar").addClass("show");
-    });
-    $(".close_btn").click(function() {
-      $(".nav_bar").removeClass("show");
-    });
-  });
